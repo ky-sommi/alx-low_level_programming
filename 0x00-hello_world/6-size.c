4f308295c8eca25c9b8c230018cb1b181065a5a3
ky-sommi <chiweitekingsley@gmail.com>
2022-09-09 04:29:24 -0700

    My first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print variable type
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of char:%d byte(s)\n",sizeof(a));
+	printf("size of int:%d byte(s)\n", sizeof(b));
+	printf("size of long int:%d byte(s)\n",sizeof(c));
+	printf("size of long long int:%d byte(s)\n",sizeof(d));
+	printf("size of float:%d byte(s)\n",sizeof(e));
+	return (0);
+}
+
