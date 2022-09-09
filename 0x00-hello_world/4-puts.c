deb8a9587c90a953af3f43dfd7ce8804e8c5e7f4
ky-sommi <chiweitekingsley@gmail.com>
2022-09-09 06:09:47 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /** 
- * main - entry point
+ * main - Entry point
  *
  * Return: 0 successful 
  */
+
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
