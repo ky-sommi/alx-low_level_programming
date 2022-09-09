f5a922cca16b42def23f7955ef07f4c34df8eabf
ky-sommi <chiweitekingsley@gmail.com>
2022-09-09 05:50:42 -0700

    My first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-
 /**
- * main - Print text
+ * main - Entry point
  *
- * Return : Always 0 (success)
+ * Return: Always (0) success
  *
  */
 
-int main(void)
+int main(void) 
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
-
-}	
+}
